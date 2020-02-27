b5f38f903279d0f5b330f741f058f1e6ddc9efac
olive-okamoto <oliveoil0211@gmail.com>
2020-02-27 16:43:41 +0900

    Add an extension to create a nil filter for ObservableType

---
+++ anistory/Extensions/Rx/ObservableType+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  ObservableType+Extension.swift
+//  anistory
+//
+//  Created by RyoOkamto on 2020/02/26.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import RxSwift
+
+public protocol OptionalType {
+    associatedtype Wrapped
+    var value: Wrapped? { get }
+}
+
+extension Optional: OptionalType {
+    public var value: Wrapped? {
+        return self
+    }
+}
+
+extension ObservableType where Element: OptionalType {
+    func filterNil() -> Observable<Element.Wrapped> {
+        return self.flatMap { element -> Observable<Element.Wrapped> in
+            guard let value = element.value else {
+                return Observable<Element.Wrapped>.empty()
+            }
+            return Observable<Element.Wrapped>.just(value)
+        }
+    }
+}
