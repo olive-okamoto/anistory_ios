1777fd10685544567f56738671d7307166926ab1
olive-okamoto <oliveoil0211@gmail.com>
2020-03-01 15:22:09 +0900

    Define a method to inject necessary data to classes and return a module

--- anistory/Modules/Profile/Router/ProfileRouter.swift
+++ anistory/Modules/Profile/Router/ProfileRouter.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import SwiftUI
+
+class ProfileRouter: ProfilePresenterToRouterProtocol {
+    
+    func associatedView() -> AnyView {
+        let interactor = ProfileInteractor()
+        let presenter = ProfilePresenter(interactor: interactor, router: self)
+        let viewInput = ProfileViewModel(presenter: presenter)
+        let view = MyProfileView(viewModel: viewInput)
+        interactor.setup(presenter: presenter)
+        presenter.setup(viewInput: viewInput)
+        return AnyView(view)
+    }
+    
+}
