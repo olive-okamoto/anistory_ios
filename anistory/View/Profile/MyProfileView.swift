c64eddf519dd076c7b6d23bb34ef6cd8d9c9abfd
olive-okamoto <oliveoil0211@gmail.com>
2020-03-01 15:18:59 +0900

    Display a profile data when a subscribed data is updated

--- anistory/View/Profile/MyProfileView.swift
+++ anistory/View/Profile/MyProfileView.swift
@@ -9,22 +9,41 @@
 import SwiftUI
 
 struct MyProfileView: View {
+    
+    @ObservedObject var viewModel: ProfileViewModel
+    
+    init(viewModel: ProfileViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
         VStack(alignment: .center) {
             CircleThumbnailImage()
             
             Text("Name").font(.title)
-            HStack {
-                Text("Record")
-                Text("Followings")
-                Text("Follwers")
-            }
+            viewModel.profileData.map({ profile in
+                HStack {
+                    VStack {
+                        Text("Record")
+                        Text(String(profile.recordsCount))
+                    }
+                    VStack {
+                        Text("Followings")
+                        Text(String(profile.followingsCount))
+                    }
+                    VStack {
+                        Text("Follwers")
+                        Text(String(profile.followersCount))
+                    }
+                }
+            })
         }.frame(minWidth: 0, maxWidth: UIScreen.main.bounds.width, minHeight: 0, maxHeight: .infinity, alignment: .top)
+        .onAppear(perform: viewModel.fetchProfile)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        MyProfileView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MyProfileView()
+//    }
+//}
