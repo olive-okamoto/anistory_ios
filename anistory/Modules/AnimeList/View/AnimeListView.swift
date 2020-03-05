20bda008ed2d13e3c859e06c9859008f95fb836f
olive-okamoto <oliveoil0211@gmail.com>
2020-03-05 17:04:52 +0900

    Create a list view

---
+++ anistory/Modules/AnimeList/View/AnimeListView.swift
@@ -0,0 +1,21 @@
+//
+//  AnimeListView.swift
+//  anistory
+//
+//  Created by RyoOkamto on 2020/03/05.
+//  Copyright © 2020 oliveapps. All rights reserved.
+//
+
+import SwiftUI
+
+struct AnimeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimeListView()
+    }
+}
