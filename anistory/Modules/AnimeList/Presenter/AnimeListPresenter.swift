7bf1755e8b0d8c8c8dae4c4752ba6fc8a016cfdc
olive-okamoto <oliveoil0211@gmail.com>
2020-03-06 12:04:29 +0900

    Inject necessary protocols and request anime list to an interactor in a presenter class

--- anistory/Modules/AnimeList/Presenter/AnimeListPresenter.swift
+++ anistory/Modules/AnimeList/Presenter/AnimeListPresenter.swift
@@ -7,9 +7,10 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol AnimeListViewToPresenterProtocol {
-    
+    func fetch()
 }
 
 protocol AnimeListInteractorToPresenterProtocol {
@@ -17,5 +18,25 @@ protocol AnimeListInteractorToPresenterProtocol {
 }
 
 class AnimeListPresenter {
+    private var viewInput: AnimeListPresenterToViewProtocol?
+    private var interactor: AnimeListPresenterToInteractorProtocol?
     
+    private let disposeBag = DisposeBag()
+    
+    func inject(viewInput: AnimeListPresenterToViewProtocol, interactor: AnimeListPresenterToInteractorProtocol) {
+        self.viewInput = viewInput
+        self.interactor = interactor
+    }
+}
+
+extension AnimeListPresenter: AnimeListViewToPresenterProtocol {
+    func fetch() {
+        interactor?.fetch()
+            .subscribe(
+                onNext: { [weak self] list in
+                    guard let self = self else { return }
+                    self.viewInput?.setList(list.nodes)
+            }, onError: nil, onCompleted: nil, onDisposed: nil)
+            .disposed(by: disposeBag)
+    }
 }
