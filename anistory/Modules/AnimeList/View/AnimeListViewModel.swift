fff890311ad9ff0d3145481ae79daf84f413f2b4
olive-okamoto <oliveoil0211@gmail.com>
2020-03-06 12:01:52 +0900

    Create a view model class and protocol to request fetching data and notify a view to display results

--- anistory/Modules/AnimeList/View/AnimeListViewModel.swift
+++ anistory/Modules/AnimeList/View/AnimeListViewModel.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 oliveapps. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+protocol AnimeListPresenterToViewProtocol {
+    func setList(_ list: [SearchWorksQuery.Data.SearchWork.Node?]?)
+}
+
+class AnimeListViewModel: ObservableObject {
+    private var presenter: AnimeListViewToPresenterProtocol?
+    @Published var list: [SearchWorksQuery.Data.SearchWork.Node?]?
+    
+    func fetch() {
+        presenter?.fetch()
+    }
+}
+
+extension AnimeListViewModel: AnimeListPresenterToViewProtocol {
+    func setList(_ list: [SearchWorksQuery.Data.SearchWork.Node?]?) {
+        self.list = list
+    }
+}
